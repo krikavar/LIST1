073babc66b6c4155ada573fbc00a7b19bc533a86
Radek Křikava <krikavar.03@spst.cz>
2022-04-26 23:12:50 +0200

    Přidejte soubory projektu.

---
+++ LIST1/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace LIST1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(131, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "KLIKNI";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 41);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(100, 324);
+            this.listBox1.TabIndex = 2;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(131, 41);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(111, 324);
+            this.listBox2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(267, 377);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Úkol 1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ LIST1/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LIST1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<int> radek = new List<int>();
+        Random rng = new Random();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int cislo = int.Parse(textBox1.Text);
+            
+            //random
+            for (int i = 0; i < int.Parse(textBox1.Text); i++)
+            {
+                int nahoda = rng.Next(19, 31);
+                radek.Add(nahoda);
+            }
+
+            //prvocislo
+            bool Prvocislo(int c2)
+            {
+                int delitelu = 0;
+                for (int i = 1; i <= c2; i++)
+                {
+                    if (c2 % i == 0)
+                    {
+                        delitelu++; 
+                    }
+                }
+                if (delitelu <= 2) 
+                { 
+                    return true;
+                }
+                else 
+                { 
+                    return false; 
+                }
+            }
+
+           
+            bool first = true;
+            foreach (int c3 in radek)
+            {
+                listBox1.Items.Add(c3.ToString());
+                if (first == true)
+                {
+                    if (Prvocislo(c3))
+                    {
+                        first = false;
+                        MessageBox.Show("Prvocislo: " + c3);
+                    }
+                }
+            }
+
+            //metoda
+            void metoda(List<int> list, ListBox listBox)
+            {
+                listBox1.Items.Clear();
+                foreach (int c in list)
+                {
+                    listBox.Items.Add(c.ToString());
+                }
+            }
+
+            MessageBox.Show("Prvni pozice max cisla v listu: " + radek.IndexOf(radek.Max()));
+            MessageBox.Show("Posledni pozice max cisla v listu: " + radek.LastIndexOf(radek.Max()));
+            MessageBox.Show("Prumer: " + radek.Average().ToString());
+            metoda(radek, listBox1);
+
+            List<int> neduplicita = radek.Distinct().ToList();
+            metoda(neduplicita, listBox2);
+        }
+    }
+}
